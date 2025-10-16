980c6dadc3a646242a740ecf77de240dd8cb0e8e
Duncanmaina58 <dun.can.duntez@gmail.com>
2025-10-16 13:12:49 +0300

    Connected frontend to Render backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Jobs from "./pages/Jobs";
